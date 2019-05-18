3fa73fb7fb00f036f392df43430733179305dc23
Tom Allen <tswallen@outlook.com>
2019-05-18 13:50:47 +1000

    New changes

--- main.py
+++ main.py
@@ -1,31 +1,66 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from multiprocessing import Process
+from tkinter import *
 
-joints = np.array([
-    (30, 6),
-    (90, 5),
-    (20, 3)
-],
-    dtype=[('angle', 'f4'), ('length', 'f4')]
-    )
+master = Tk()
 
-points = np.array([
-    (0, 0)
-],
-    dtype=[('x', 'f4'), ('y', 'f4')]
-    )
+joints = np.array(
+    [
+        (0, 0),
+        (75, 6),
+        (40, 3),
+        (270, 2)
+    ],
+    dtype=[
+        ('angle', 'f4'),
+        ('length', 'f4')]
+)
 
-points2 = []
+points = np.zeros(
+    (
+        joints.size,
+    ),
+    dtype=[
+        ('x', 'f4'),
+        ('y', 'f4')
+    ]
+)
 
-for index, (angle, length) in np.ndenumerate(joints):
-    
-    x = length * np.cos(angle)
-    y = length * np.sin(angle)
+w = Scale(master, from_=0, to=100, variable=joints[1][0])
+w.pack()
 
-    points2.append((x,y))
+l = Label(master, textvariable=joints[1][0])
+l.pack()
 
+def test():
+    print(joints)
 
-print(points2)
+b = Button(master, command=test)
+b.pack()
 
+def renderVisualisation():
+    x_cache, y_cache = 0.0, 0.0
+    for index, (angle, length) in np.ndenumerate(joints):
 
-plt.show()
+        x = np.add(np.multiply(length, np.cos(np.radians(angle))), x_cache)
+        y = np.add(np.multiply(length, np.sin(np.radians(angle))), y_cache)
+
+        points[index] = (x, y)
+
+        x_cache, y_cache = x, y
+
+    x, y = zip(*points)
+    plt.xlim(0,10)
+    plt.ylim(0,10)
+    plt.plot(x, y)
+    plt.show()
+
+def renderUI():
+    mainloop()
+
+def moveJoint(joint, value):
+    joints[(joint,)][0] = value
+
+if __name__ == '__main__':
+    Process(target=renderVisualisation).start()
